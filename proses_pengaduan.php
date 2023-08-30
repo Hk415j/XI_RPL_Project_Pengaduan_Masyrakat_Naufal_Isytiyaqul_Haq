ad195951fa5da97e9866d98874bdf813f77c426e
hk416 <125108433+Hk415j@users.noreply.github.com>
2023-08-30 10:46:49 +0700

    Add files via upload

--- proses_pengaduan.php
+++ proses_pengaduan.php
@@ -1,4 +1,25 @@
 <?php
-echo $_FILES['foto']['nama'];
-echo $_FILES['foto']['tnp_nama'];
-?>
+include "../koneksi.php";
+$status = '0';
+$isi = $_POST['isi_laporan'];
+$nik = '039151';
+$nama_foto = $_FILES['foto']['name'];
+$asal_foto =$_FILES['foto']['tmp_name'];
+move_uploaded_file($asal_foto, "image/$nama_foto");
+
+$proses_pengaduan = $koneksi->query("INSERT INTO `pengaduan` VALUES ('',NOW(),'$nik','$isi','$nama_foto','$status')");
+if($proses_pengaduan){
+    echo "
+        <script>
+            alert('Laporan telah berhasil dilakukan');
+            document.location.href = '../laporan/laporan.php';
+        </script>
+    ";
+} else {
+    echo "
+        <script>
+            alert('Laporan telah berhasil dilakukan');
+            document.location.href = 'pengaduan.php';
+        </script>
+    ";
+}
